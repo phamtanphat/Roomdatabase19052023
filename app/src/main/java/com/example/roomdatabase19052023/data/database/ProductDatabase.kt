13a35e13be9476d0ec71ac514f8c84f4121b43d8
phamtanphat <phatdroid94@gmail.com>
2023-09-11 18:34:40 +0700

    create database

--- app/src/main/java/com/example/roomdatabase19052023/data/database/ProductDatabase.kt
+++ app/src/main/java/com/example/roomdatabase19052023/data/database/ProductDatabase.kt
@@ -1,9 +1,19 @@
 package com.example.roomdatabase19052023.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [ProductEntity::class], version = 1)
 abstract class ProductDatabase: RoomDatabase(){
+    abstract fun productDAO(): ProductDAO
 
+    fun getDatabase(context: Context): ProductDatabase {
+        return Room.databaseBuilder(
+            context = context,
+            klass = ProductDatabase::class.java,
+            name = "product-database"
+        ).build()
+    }
 }
